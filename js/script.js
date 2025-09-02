dceaad40ae60065f0b5a51070754ffb204780468
AhmedMDR77 <134991301+AhmedMDR77@users.noreply.github.com>
2025-09-01 22:56:08 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -91,7 +91,7 @@ function setAll(imdb, title, season, episode, type) {
     a.setAttribute("IMDB", imdb);
     a.setAttribute(
       "href",
-      `https://www.2embed.to/embed/imdb/tv?id=${imdb}&s=${season}&e=${episode}`
+      `https://www.2embed.cc/embed/imdb/tv?id=${imdb}&s=${season}&e=${episode}`
     );
     a.setAttribute("target", "_blank");
     a.click();
